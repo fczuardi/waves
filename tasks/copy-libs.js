df79dc85a2af7f7ff6ae36e8db6f7bb891c70d22
Fabricio C Zuardi <fabricio@fabricio.org>
2017-01-21 11:22:59 -0200

    add pixi as a dependency for rendering 2d sprites

--- tasks/copy-libs.js
+++ tasks/copy-libs.js
@@ -2,7 +2,9 @@ require('shelljs/global');
 
 var LIBDIR = './docs/js/lib/';
 var MATTERJS = './node_modules/matter-js/build/matter-dev.js';
+var PIXIJS = './node_modules/pixi.js/dist/pixi.js';
 
 mkdir('-p', LIBDIR);
 cp(MATTERJS, LIBDIR + 'matter.js');
+cp(PIXIJS, LIBDIR + 'pixi.js');
 
