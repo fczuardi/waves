2bd683003e9f52573909962b04730074e5752b9e
Fabricio C Zuardi <fabricio@fabricio.org>
2017-01-21 00:53:40 -0200

    getting started example and a first colision test

--- tasks/copy-libs.js
+++ tasks/copy-libs.js
@@ -4,5 +4,5 @@ var LIBDIR = './docs/js/lib/';
 var MATTERJS = './node_modules/matter-js/build/matter-dev.js';
 
 mkdir('-p', LIBDIR);
-cp(MATTERJS, LIBDIR + '.');
+cp(MATTERJS, LIBDIR + 'matter.js');
 
