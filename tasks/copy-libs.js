b70bc465e2e7254bbde7e9476ce64e2e8bd266ce
Flavio Heleno <flaviohbatista@gmail.com>
2017-01-21 19:12:58 -0200

    3 types of rock and mobile support

--- tasks/copy-libs.js
+++ tasks/copy-libs.js
@@ -3,8 +3,9 @@ require('shelljs/global');
 var LIBDIR = './docs/js/lib/';
 var MATTERJS = './node_modules/matter-js/build/matter-dev.js';
 var PIXIJS = './node_modules/pixi.js/dist/pixi.js';
+var FASTCLICKJS = './node_modules/fastclick/lib/fastclick.js';
 
 mkdir('-p', LIBDIR);
 cp(MATTERJS, LIBDIR + 'matter.js');
 cp(PIXIJS, LIBDIR + 'pixi.js');
-
+cp(FASTCLICKJS, LIBDIR + 'fastclick.js');
